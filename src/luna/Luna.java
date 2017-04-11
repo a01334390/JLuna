1d88cfb070d3c40fa8ab89d234e5283d485a5ef1
A01334390 <martingarciadelangel@me.com>
2017-04-10 22:51:51 -0500

    Luna Initial Commit
    Started from the bottom, now we are here

---
+++ src/luna/Luna.java
@@ -0,0 +1,21 @@
+/*
+ * Built in 2016© By Code Managers in Kraken Tech Ltd.
+ * Compiled and Revised by Advisors outside the company
+ * Refer to documentation attached for any other reference on code or anything related to this Source Code
+ */
+package luna;
+
+/**
+ *
+ * @author a01334390
+ */
+public class Luna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
